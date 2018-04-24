5e381b461c25470947e3c007c1665153458c7fab
Cláudio Pereira <git@claudiop.com>
2018-04-24 16:30:01 +0100

    Change hardcoded user choosable values to start at index 0.

--- users/models.py
+++ users/models.py
@@ -15,11 +15,11 @@ class User(AbstractUser):
     residence = TextField(max_length=50, null=True, blank=True, verbose_name='Residência')
     picture = ImageField(upload_to=user_directory_path, null=True, blank=True, verbose_name='Foto')
 
-    HIDDEN = 1  # No profile at all
-    LIMITED = 2  # Show limited information, only to users
-    USERS = 3  # Show full profile, only to users
-    MIXED = 4  # Show limited information to visitors, full to users
-    PUBLIC = 5  # Show everything to everyone
+    HIDDEN = 0  # No profile at all
+    LIMITED = 1  # Show limited information, only to users
+    USERS = 2  # Show full profile, only to users
+    MIXED = 3  # Show limited information to visitors, full to users
+    PUBLIC = 4  # Show everything to everyone
 
     PROFILE_VISIBILITY_CHOICES = (
         (HIDDEN, 'Oculto'),
@@ -28,11 +28,11 @@ class User(AbstractUser):
         (MIXED, 'Misto'),
         (PUBLIC, 'Público'),
     )
-    profile_visibility = IntegerField(choices=PROFILE_VISIBILITY_CHOICES, default=1)
+    profile_visibility = IntegerField(choices=PROFILE_VISIBILITY_CHOICES, default=0)
 
-    MALE = 1
-    FEMALE = 2
-    MULTIPLEGENDERS = 3
+    MALE = 0
+    FEMALE = 1
+    MULTIPLEGENDERS = 2
 
     GENDER_CHOICES = (
         (MALE, 'Homem'),
@@ -55,21 +55,21 @@ class UserBadge(Model):
 
 
 class SocialNetworkAccount(Model):
-    GITLAB = 1
-    GITHUB = 2
-    REDDIT = 3
-    DISCORD = 4
-    LINKEDIN = 5
-    TWITTER = 6
-    GOOGLEPLUS = 7
-    FACEBOOK = 8
-    VIMEO = 9
-    YOUTUBE = 10
-    DEVIANTART = 11
-    INSTAGRAM = 12
-    FLICKR = 13
-    MYANIMELIST = 14
-    IMDB = 15
+    GITLAB = 0
+    GITHUB = 1
+    REDDIT = 2
+    DISCORD = 3
+    LINKEDIN = 4
+    TWITTER = 5
+    GOOGLEPLUS = 6
+    FACEBOOK = 7
+    VIMEO = 8
+    YOUTUBE = 9
+    DEVIANTART = 10
+    INSTAGRAM = 11
+    FLICKR = 12
+    MYANIMELIST = 13
+    IMDB = 14
 
     SOCIAL_NETWORK_CHOICES = (
         (GITLAB, 'GitLab'),
@@ -77,6 +77,7 @@ class SocialNetworkAccount(Model):
         (REDDIT, 'Reddit'),
         (DISCORD, 'Discord'),
         (LINKEDIN, 'Linkedin'),
+        (TWITTER, 'Twitter'),
         (GOOGLEPLUS, 'Google+'),
         (FACEBOOK, 'Facebook'),
         (VIMEO, 'Vimeo'),
@@ -85,8 +86,11 @@ class SocialNetworkAccount(Model):
         (INSTAGRAM, 'Instagram'),
         (FLICKR, 'Flickr'),
         (MYANIMELIST, 'MyAnimeList'),
-        (MYANIMELIST, 'IMDB'),
+        (IMDB, 'IMDB'),
     )
-    user = ForeignKey(User, on_delete=models.CASCADE)
+    user = ForeignKey(User, on_delete=models.CASCADE, related_name='social_networks')
     network = IntegerField(choices=SOCIAL_NETWORK_CHOICES)
     profile = TextField(max_length=200)
+
+    def __str__(self):
+        return f'{self.SOCIAL_NETWORK_CHOICES[self.network][1]}: {self.profile} ({self.user})'
