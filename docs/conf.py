1bc4b969de95ea339e8e3bd0957b605dc6022bfc
Cláudio Pereira <git@claudiop.com>
2019-03-09 21:40:15 +0000

    Change settings_module to match new location. Move wsgi file to the root.

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import sys
 
 import django
 sys.path.insert(0, os.path.abspath('..'))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kleep.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 django.setup()
 
 
