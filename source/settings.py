0ef73d11b21760d784e987ab1a81f67ce976e6a4
Cláudio Pereira <git@claudiop.com>
2019-03-09 18:02:53 +0000

    Change settings.py file with better external config loading, different static settings, logging disabled (was broken).

--- source/settings.py
+++ source/settings.py
@@ -6,28 +6,6 @@ from django.urls import reverse
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-with open(BASE_DIR + '/settings.json') as json_data:
-    settings = json.load(json_data)
-    SECRET_KEY = settings['SECRET_KEY']
-    CLIP_USERNAME = settings['CLIP_USERNAME']
-    CLIP_PASSWORD = settings['CLIP_PASSWORD']
-    EMAIL_SERVER = settings['EMAIL_SERVER']
-    EMAIL_SUFFIX = settings['EMAIL_SUFFIX']
-    EMAIL_ACCOUNT = settings['EMAIL_ACCOUNT']
-    EMAIL_PASSWORD = settings['EMAIL_PASSWORD']
-    DATABASES = settings['DATABASES']
-    PIWIK_DOMAIN_PATH = settings['PIWIK_DOMAIN_PATH']
-    PIWIK_SITE_ID = settings['PIWIK_SITE_ID']
-    ALLOWED_HOSTS = settings['HOSTS']
-
-DEBUG = False
-if not DEBUG:
-    CSRF_COOKIE_SECURE = True
-    SESSION_COOKIE_SECURE = True
-else:
-    CSRF_COOKIE_SECURE = False
-    SESSION_COOKIE_SECURE = False
-
 # Application definition
 INSTALLED_APPS = [
     'dal',
@@ -112,16 +90,21 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 LANGUAGE_CODE = 'pt-pt'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Static files
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+STATICFILES_DIRS = [
+    "static"
+]
+
 STATIC_URL = '/static/'
 STATIC_ROOT = '/srv/http/kleep/static/'
 MEDIA_URL = '/media/'
@@ -132,29 +115,29 @@ CKEDITOR_BASEPATH = '/static/ckeditor/ckeditor/'
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 CKEDITOR_ALLOW_NONIMAGE_FILES = True
 CKEDITOR_IMAGE_BACKEND = 'pillow'
-CKEDITOR_MATHJAX_URL='//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/MathJax.js?config=TeX-AMS_HTML'
+CKEDITOR_MATHJAX_URL = '//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/MathJax.js?config=TeX-AMS_HTML'
 CKEDITOR_SIMPLE_TOOLBAR = [
-            {'name': 'styles', 'items': ['Styles']},
-            {'name': 'basicstyles',
-             'items': ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']},
-            {'name': 'colors', 'items': ['TextColor', 'BGColor']},
-            {'name': 'paragraph', 'items': ['NumberedList', 'BulletedList', 'Blockquote']},
-            {'name': 'insert', 'items': ['Image', 'Table', 'Mathjax', 'CodeSnippet']},
-            {'name': 'tools', 'items': ['Source', 'Preview', 'Maximize']}
-        ]
+    {'name': 'styles', 'items': ['Styles']},
+    {'name': 'basicstyles',
+     'items': ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']},
+    {'name': 'colors', 'items': ['TextColor', 'BGColor']},
+    {'name': 'paragraph', 'items': ['NumberedList', 'BulletedList', 'Blockquote']},
+    {'name': 'insert', 'items': ['Image', 'Table', 'Mathjax', 'CodeSnippet']},
+    {'name': 'tools', 'items': ['Source', 'Preview', 'Maximize']}
+]
 CKEDITOR_COMPLEX_TOOLBAR = [
-            {'name': 'styles', 'items': ['Styles', 'Format',  'FontSize']},
-            {'name': 'basicstyles',
-             'items': ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']},
-            {'name': 'colors', 'items': ['TextColor', 'BGColor']},
-            {'name': 'paragraph',
-             'items': ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-',
-                       'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']},
-            {'name': 'links', 'items': ['Link', 'Unlink']},
-            {'name': 'insert',
-             'items': ['Image', 'Table', 'HorizontalRule', 'SpecialChar', 'Mathjax', 'CodeSnippet', 'Iframe']},
-            {'name': 'tools', 'items': ['ShowBlocks', 'Source', 'Preview', 'Maximize']}
-        ]
+    {'name': 'styles', 'items': ['Styles', 'Format', 'FontSize']},
+    {'name': 'basicstyles',
+     'items': ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']},
+    {'name': 'colors', 'items': ['TextColor', 'BGColor']},
+    {'name': 'paragraph',
+     'items': ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-',
+               'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']},
+    {'name': 'links', 'items': ['Link', 'Unlink']},
+    {'name': 'insert',
+     'items': ['Image', 'Table', 'HorizontalRule', 'SpecialChar', 'Mathjax', 'CodeSnippet', 'Iframe']},
+    {'name': 'tools', 'items': ['ShowBlocks', 'Source', 'Preview', 'Maximize']}
+]
 CKEDITOR_EXTRA_PLUGINS = 'uploadimage,mathjax,codesnippet,div,autolink,autoembed,embedsemantic,' \
                          'autogrow,widget,lineutils,clipboard,dialog,dialogui,elementspath'
 CKEDITOR_CONFIGS = {
@@ -187,7 +170,7 @@ VERSION = subprocess.check_output([
     "HEAD"
 ]).decode('ascii')
 
-REGISTRATIONS_ENABLED = True
+REGISTRATIONS_ENABLED = False
 REGISTRATIONS_ATTEMPTS_TOKEN = 3
 REGISTRATIONS_TIMEWINDOW = 60  # Minutes
 REGISTRATIONS_TOKEN_LENGTH = 6
@@ -202,26 +185,39 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 100
 }
 
-VULNERABILITY_CHECKING = 'vulnerabilities' in DATABASES
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/var/log/kleep.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'propagate': True,
-        },
-    },
-}
-if DEBUG:
-    LOGGING['loggers']['django']['level'] = 'DEBUG'
-
 LOGIN_URL = '/entrar'
+
+LEAFLET_CONFIG = {
+    'SPATIAL_EXTENT': (-9.20209, 38.65727, -9.20955, 38.66384),
+    'DEFAULT_CENTER': (38.6608, -9.205576),
+    'DEFAULT_ZOOM': 18,
+    'MIN_ZOOM': 16,
+    'MAX_ZOOM': 19,
+    'TILES': 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+    'ATTRIBUTION_PREFIX': 'Leaflet',
+    'RESET_VIEW': False,
+}
+
+DEBUG = False
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+VULNERABILITY_CHECKING = False
+
+# LOGGING = {}  # TODO
+
+assert 'KLEEP_CONFIG' in os.environ
+CONFIG_PATH = os.environ['KLEEP_CONFIG']
+assert os.path.isfile(CONFIG_PATH)
+
+with open(CONFIG_PATH) as file:
+    locals().update(json.load(file))
+
+if DEBUG:
+    CSRF_COOKIE_SECURE = False
+    SESSION_COOKIE_SECURE = False
+    # LOGGING['loggers']['django']['level'] = 'DEBUG'
+
+try:
+    VULNERABILITY_CHECKING = 'vulnerabilities' in DATABASES
+except NameError:
+    pass
