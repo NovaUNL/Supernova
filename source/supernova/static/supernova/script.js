7a205780e2e7c87d0f30025dfff4798ebc4dcbe8
Cláudio Pereira <git@claudiop.com>
2020-01-05 03:45:44 +0000

    Mobile support. CSS cleanup

--- source/supernova/static/supernova/script.js
+++ source/supernova/static/supernova/script.js
@@ -1,7 +1,8 @@
-function authShowPrompt() {
-    document.getElementById("overlay").style.display = 'block';
-}
-
-function overlayClose() {
-    document.getElementById("overlay").style.display = 'none';
+function toggleMenu() {
+    let element = document.getElementById("nav-column");
+    if (element.style.display !== 'block') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
 }
