96c2670988689327390d58413ea1945c4d44c016
Cláudio Pereira <git@claudiop.com>
2020-01-28 22:13:50 +0000

    Free rooms in index page

--- source/supernova/views.py
+++ source/supernova/views.py
@@ -3,6 +3,7 @@ import random
 from itertools import chain
 
 from django.core.cache import cache
+from django.db.models import Q, Sum, F
 from django.shortcuts import render
 
 from scrapper.boinc import boincstats
@@ -10,16 +11,20 @@ from scrapper.transportation import mts, tst
 from services.utils import get_next_meal_items
 from users.forms import LoginForm
 from supernova.models import Changelog, Catchphrase
-from settings import VERSION
 from news.models import NewsItem
+from college import models as college
 
 
 def index(request):
     context = build_base_context(request)
+
+    now = datetime.now()
+    midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    minutes = (now - midnight).seconds // 60
+
     context['title'] = "Perfeição nanométrica"
     context['news'] = NewsItem.objects.order_by('datetime').reverse()[0:6]
     context['changelog'] = Changelog.objects.order_by('date').reverse()[0:3]
-    context['catchphrase'] = random.choice(Catchphrase.objects.all())
     context['meal_items'], context['meal_date'], time = get_next_meal_items()
     context['meal_name'] = 'Almoço' if time == 2 else 'Jantar'
     boinc_users = boincstats.get_team_users(2068385380)
@@ -31,19 +36,27 @@ def index(request):
     departures = cache.get('departures')
     if departures is None:
         departures = list(chain(mts.get_departure_times(), tst.get_departure_times()))
+        curr_time = now.time()
         for departure in departures:
-            today = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
-            now = datetime.now().time()
             departure_time = departure['time']
-            if now < departure_time:
-                departure['datetime'] = today + timedelta(hours=departure_time.hour, minutes=departure_time.minute)
+            if curr_time < departure_time:
+                departure['datetime'] = midnight + timedelta(
+                    hours=departure_time.hour, minutes=departure_time.minute)
             else:
-                departure['datetime'] = today + timedelta(days=1, hours=departure_time.hour,
-                                                          minutes=departure_time.minute)
+                departure['datetime'] = midnight + timedelta(
+                    days=1, hours=departure_time.hour, minutes=departure_time.minute)
         departures.sort(key=lambda departure: departure['datetime'])
         departures = departures[:5]
-        cache.set('departures', departures, timeout=60)
+        cache.set('departures', departures, timeout=60 * 10)
     context['departures'] = departures
+
+    context['free_rooms'] = college.Room.objects \
+        .annotate(turn_instances__end=F('turn_instances__start') + F('turn_instances__duration')) \
+        .filter(unlocked=True) \
+        .select_related('building') \
+        .order_by('building__abbreviation', 'name') \
+        .exclude(Q(turn_instances__start__lt=minutes) & Q(turn_instances__end__gt=minutes)) \
+        .distinct('building__abbreviation', 'name')
 
     return render(request, 'supernova/index.html', context)
 
@@ -52,6 +65,7 @@ def build_base_context(request):
     result = {
         'disable_auth': False,
         'sub_nav': None,
+        'catchphrase': random.choice(Catchphrase.objects.all())
     }
     if not request.user.is_authenticated:
         result['login_form'] = LoginForm()
