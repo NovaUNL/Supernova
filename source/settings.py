1e9f31a3265056f4bc66dde1823bd0ed89d7eb25
Cláudio Pereira <git@claudiop.com>
2020-01-29 00:43:18 +0000

    Add redis as a cache backend

--- source/settings.py
+++ source/settings.py
@@ -98,6 +98,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 # Internationalization
 LANGUAGE_CODE = 'pt-pt'
 TIME_ZONE = 'UTC'
