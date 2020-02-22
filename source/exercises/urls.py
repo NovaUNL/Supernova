5592b5e8d935ab8593eceb8ff3d9deef071f5d3b
Cláudio Pereira <git@claudiop.com>
2020-02-22 16:55:29 +0000

    Exercises placeholder

--- source/exercises/urls.py
+++ source/exercises/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'exercises'
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('novo/', views.create_exercise, name='create_exercise'),
     path('novo/simples', views.create_qa_exercise, name='create_qa_exercise'),
     path('novo/escolha_multipla', views.create_mc_exercise, name='create_mc_exercise'),
--- source/exercises/views.py
+++ source/exercises/views.py
@@ -5,10 +5,18 @@ from exercises.forms import ExerciseForm, AnswerFormSet
 from supernova.views import build_base_context
 
 
+def index(request):
+    context = build_base_context(request)
+    context['title'] = 'Exercicios'
+    context['msg_title'] = 'Por fazer'
+    context['msg_content'] = 'Esta funcionalidade ainda está inacabada'
+    context['sub_nav'] = [{'name': 'Exercicios', 'url': reverse('exercises:index')}]
+    return render(request, 'supernova/message.html', context)
+
 def create_exercise(request):
     context = build_base_context(request)
     context['title'] = 'Submeter exercício'
-    context['sub_nav'] = [{'name': 'Exercicios', 'url': '#TODO'},
+    context['sub_nav'] = [{'name': 'Exercicios', 'url': reverse('exercises:index')},
                           {'name': 'Submeter exercício', 'url': reverse('exercises:create_exercise')}]
     return render(request, 'exercises/pre_submit.html', context)
 
@@ -17,7 +25,7 @@ def create_qa_exercise(request):
     context = build_base_context(request)
     context['title'] = 'Submeter exercício questão-resposta'
     context['exercise_form'] = ExerciseForm()
-    context['sub_nav'] = [{'name': 'Exercicios', 'url': '#TODO'},
+    context['sub_nav'] = [{'name': 'Exercicios', 'url': reverse('exercises:index')},
                           {'name': 'Submeter exercício', 'url': reverse('exercises:create_exercise')},
                           {'name': 'Questão-resposta', 'url': '#'}]
     return render(request, 'exercises/qa_submit.html', context)
@@ -28,7 +36,7 @@ def create_mc_exercise(request):
     context['title'] = 'Submeter exercício de escolha múltipla'
     context['exercise_form'] = ExerciseForm()
     context['answers_formset'] = AnswerFormSet()
-    context['sub_nav'] = [{'name': 'Exercicios', 'url': '#TODO'},
+    context['sub_nav'] = [{'name': 'Exercicios', 'url': reverse('exercises:index')},
                           {'name': 'Submeter exercício', 'url': reverse('exercises:create_exercise')},
                           {'name': 'Escolha múltipla', 'url': '#'}]
     return render(request, 'exercises/multiple_choice_submit.html', context)
