d53dbed7d8731ecdb0cdc6d2c173db9d67f4fb8f
Cláudio Pereira <git@claudiop.com>
2020-02-25 03:55:05 +0000

    Template tags to change string case

--- source/supernova/templatetags/strutils.py
+++ source/supernova/templatetags/strutils.py
@@ -8,3 +8,10 @@ def startswith(text, starts):
     if isinstance(text, str):
         return text.startswith(starts)
     return False
+
+
+@register.filter('endswith')
+def endswith(text, starts):
+    if isinstance(text, str):
+        return text.endswith(starts)
+    return False
