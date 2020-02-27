6fa2fa9356cf4e9dc1064125763f9bf13936705e
Cláudio Pereira <git@claudiop.com>
2020-02-27 10:51:35 +0000

    Add new slider toggle button

---
+++ source/supernova/widgets.py
@@ -0,0 +1,43 @@
+from django.forms import CheckboxInput, Widget
+from django.forms.widgets import boolean_check
+
+
+class SliderInput(Widget):
+    # input_type = 'checkbox'
+    template_name = 'widgets/slider.html'
+
+    def __init__(self, attrs=None, check_test=None):
+        super().__init__(attrs)
+        # check_test is a callable that takes a value and returns True
+        # if the checkbox should be checked for that value.
+        self.check_test = boolean_check if check_test is None else check_test
+
+    def format_value(self, value):
+        """Only return the 'value' attribute if value isn't empty."""
+        if value is True or value is False or value is None or value == '':
+            return
+        return str(value)
+
+    def get_context(self, name, value, attrs):
+        if self.check_test(value):
+            if attrs is None:
+                attrs = {}
+            attrs['checked'] = True
+        return super().get_context(name, value, attrs)
+
+    def value_from_datadict(self, data, files, name):
+        if name not in data:
+            # A missing value means False because HTML form submission does not
+            # send results for unselected checkboxes.
+            return False
+        value = data.get(name)
+        # Translate true and false strings to boolean values.
+        values = {'true': True, 'false': False}
+        if isinstance(value, str):
+            value = values.get(value.lower(), value)
+        return bool(value)
+
+    def value_omitted_from_data(self, data, files, name):
+        # HTML checkboxes don't appear in POST data if not checked, so it's
+        # never known if the value is actually omitted.
+        return False
