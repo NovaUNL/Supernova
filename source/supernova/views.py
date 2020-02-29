594cdb5161b6cac9323d2996c5a24fc83db9c1e6
Cláudio Pereira <git@claudiop.com>
2020-02-29 22:50:57 +0000

    Limit empty rooms being showed in the index

--- source/supernova/views.py
+++ source/supernova/views.py
@@ -32,13 +32,15 @@ def index(request):
     boinc_projects.sort(key=lambda x: -x['weekly'])
     context['boinc_projects'] = boinc_projects[:3]
 
-    context['free_rooms'] = college.Room.objects \
-        .annotate(turn_instances__end=F('turn_instances__start') + F('turn_instances__duration')) \
-        .filter(unlocked=True) \
-        .select_related('building') \
-        .order_by('building__abbreviation', 'name') \
-        .exclude(Q(turn_instances__start__lt=minutes) & Q(turn_instances__end__gt=minutes)) \
-        .distinct('building__abbreviation', 'name')
+    context['free_rooms'] = random.sample(
+        set(college.Room.objects
+            .annotate(turn_instances__end=F('turn_instances__start') + F('turn_instances__duration'))
+            .filter(unlocked=True)
+            .select_related('building')
+            .order_by('building__abbreviation', 'name')
+            .exclude(Q(turn_instances__start__lt=minutes) & Q(turn_instances__end__gt=minutes))
+            .distinct('building__abbreviation', 'name')),
+        10)
 
     context['activities'] = groups.Activity.objects \
                                 .select_related('group', 'announcement') \
