ddc6668a757fced0d1ce84ca1d9f127b4941665c
Cláudio Pereira <git@claudiop.com>
2020-08-17 02:48:19 +0100

    Fix gitlab star crawler

--- source/scrapper/git/gitlab.py
+++ source/scrapper/git/gitlab.py
@@ -10,7 +10,12 @@ def get_repo_stars(user='claudiop', repo='Supernova'):
 
     response = requests.get(f"https://gitlab.com/{user}/{repo}")
     soup = bs4.BeautifulSoup(response.text, 'lxml')
-    elem = soup.find(class_="count", title='Starrers')
+    elem = soup.find(class_="count", title='Starrer')
+    if elem is None:
+        # For this to happen either the code is broken or GH is down.
+        # Set the cache nonetheless to avoid sending pointless requests (once per page load)
+        cache.set('gitlab_stars', 'err', timeout=60)
+        return 'err'
     stars = int(elem.text)
     cache.set('gitlab_stars', stars, timeout=3600)
     return stars
