66c08e6f366c30bde159451b8d161ca07fe5f9b1
Cláudio Pereira <git@claudiop.com>
2020-09-30 02:52:05 +0100

    News: Move and parametrize fetchnews command.

--- source/news/management/commands/fetchnews.py
+++ source/news/management/commands/fetchnews.py
@@ -7,11 +7,19 @@ from scrapper.news import settings
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--days",
+            default=10,
+            help="Number of days back in time to crawl",
+            type=int
+        )
 
     def handle(self, *args, **options):
+        days = options['days']
         crawler_settings = Settings()
         crawler_settings.setmodule(settings)
         process = CrawlerProcess(crawler_settings)
 
-        process.crawl(NewsItemSpider, days=180)
+        process.crawl(NewsItemSpider, days=days)
         process.start()
