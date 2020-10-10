72c0f6d813479caad5028faf5165ea91acc61a75
Cláudio Pereira <git@claudiop.com>
2020-10-10 17:20:56 +0100

    Fix #15. News pagination.

--- source/news/views.py
+++ source/news/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from supernova.views import build_base_context
@@ -5,16 +6,26 @@ from news.models import NewsItem
 
 
 def index(request):
+    paginator = Paginator(NewsItem.objects.order_by('datetime').reverse(), 10)
+    p = 1
+    if 'p' in request.GET:
+        try:
+            p = int(request.GET['p'])
+        except ValueError:
+            pass
+    p = max(min(p, paginator.num_pages), 0)
+    page = paginator.page(p)
+
     context = build_base_context(request)
     context['pcode'] = 'c_news'
     context['title'] = 'Notícias'
-    context['news'] = NewsItem.objects.order_by('datetime').reverse()[0:10]
+    context['page'] = page
+    context['paginator'] = paginator
     context['sub_nav'] = [{'name': 'Noticias', 'url': reverse('news:index')}]
-    return render(request, 'news/recent.html', context)
+    return render(request, 'news/list.html', context)
 
 
 def item(request, news_item_id):
-    news_item = get_object_or_404(NewsItem, id=news_item_id)
     news_item = get_object_or_404(NewsItem, id=news_item_id)
     context = build_base_context(request)
     context['pcode'] = 'c_news_item'
