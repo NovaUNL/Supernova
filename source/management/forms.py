2ab60b157859493771470d15e8ed6053b722e0ca
Cláudio Pereira <git@claudiop.com>
2020-10-14 02:18:06 +0100

    Management: Add password reset, student assign and teacher assign features.

--- source/management/forms.py
+++ source/management/forms.py
@@ -1,7 +1,11 @@
 from dal import autocomplete
 from django import forms as djf
+from django.core.exceptions import ValidationError
+
 from supernova import models as supernova
-from users import models as users
+from users import models as users, triggers
+from college import models as college
+from users.registrations import generate_token
 
 
 class ChangelogForm(djf.ModelForm):
@@ -17,3 +21,64 @@ class ReputationOffsetForm(djf.ModelForm):
         model = users.ReputationOffset
         fields = ('receiver', 'amount', 'reason')
         widgets = {'receiver': autocomplete.ModelSelect2(url='users:nickname_ac')}
+
+
+class BindStudentToUserForm(djf.Form):
+    user = djf.IntegerField(widget=autocomplete.Select2(url='users:nickname_ac'))
+    student = djf.IntegerField(widget=autocomplete.Select2(url='college:unreg_student_ac'))
+
+    def clean_user(self):
+        user_id = self.cleaned_data.get("user")
+        return users.User.objects.filter(id=user_id).first()
+
+    def clean_teacher(self):
+        student_id = self.cleaned_data.get("student")
+        return college.Student.objects.filter(id=student_id, user=None).first()
+
+    def save(self):
+        user: users.User = self.cleaned_data.get('user')
+        student: college.Student = self.cleaned_data.get('student')
+        if student.user is None:
+            student.user = user
+            student.save(update_fields=['user'])
+            triggers.on_student_assignment(user, student)
+        else:
+            raise ValidationError(f"Student {student.name} is already assigned to user {student.user.nickname}")
+
+
+class BindTeacherToUserForm(djf.Form):
+    user = djf.IntegerField(widget=autocomplete.Select2(url='users:nickname_ac'))
+    teacher = djf.IntegerField(widget=autocomplete.Select2(url='college:unreg_teacher_ac'))
+
+    def clean_user(self):
+        user_id = self.cleaned_data.get("user")
+        return users.User.objects.filter(id=user_id).first()
+
+    def clean_teacher(self):
+        teacher_id = self.cleaned_data.get("teacher")
+        return college.Teacher.objects.filter(id=teacher_id, user=None).first()
+
+    def save(self):
+        user: users.User = self.cleaned_data.get('user')
+        teacher: college.Teacher = self.cleaned_data.get('teacher')
+        if teacher.user is None:
+            teacher.user = user
+            teacher.save(update_fields=['user'])
+            triggers.on_teacher_assignment(user, teacher)
+        else:
+            raise ValidationError(f"Teacher {teacher.name} is already assigned to user {teacher.user.nickname}")
+
+
+class PasswordResetForm(djf.Form):
+    user = djf.IntegerField(widget=autocomplete.Select2(url='users:nickname_ac'))
+
+    def clean_user(self):
+        user_id = self.cleaned_data.get("user")
+        return users.User.objects.filter(id=user_id).first()
+
+    def save(self):
+        user: users.User = self.cleaned_data.get('user')
+        new_password = generate_token(length=10)
+        user.set_password(new_password)
+        user.save()
+        return new_password
--- source/management/views.py
+++ source/management/views.py
@@ -2,7 +2,6 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 from django.urls import reverse
 
-from management.forms import ReputationOffsetForm
 from supernova import models as m
 from management import forms as f
 from supernova.views import build_base_context
@@ -29,17 +28,33 @@ def users_view(request):
         .select_related('requested_student', 'requested_teacher', 'resulting_user', 'invite') \
         .reverse()
     context['suspended_users'] = users.User.objects.order_by('nickname').filter(is_active=False).all()
+
+    reputation_offset_form = f.ReputationOffsetForm(prefix='reputation_offset_')
+    assign_student_form = f.BindStudentToUserForm(prefix='assign_student_')
+    assign_teacher_form = f.BindTeacherToUserForm(prefix='assign_teacher_')
+    password_reset_form = f.PasswordResetForm(prefix='password_reset_')
     if request.method == 'POST':
         if 'reputation_offset' in request.GET:
-            reputation_offset_form = ReputationOffsetForm(request.POST)
+            reputation_offset_form = f.ReputationOffsetForm(request.POST, prefix='reputation_offset_')
             if reputation_offset_form.is_valid():
                 offset = reputation_offset_form.save()
                 offset.issue_notification()
-        else:
-            reputation_offset_form = ReputationOffsetForm()
-    else:
-        reputation_offset_form = ReputationOffsetForm()
+        elif 'assign_student' in request.GET:
+            assign_student_form = f.BindStudentToUserForm(request.POST, prefix='assign_student_')
+            if assign_student_form.is_valid():
+                assign_student_form.save()
+        elif 'assign_teacher' in request.GET:
+            assign_teacher_form = f.BindTeacherToUserForm(request.POST, prefix='assign_teacher_')
+            if assign_teacher_form.is_valid():
+                assign_teacher_form.save()
+        elif 'password_reset' in request.GET:
+            password_reset_form = f.PasswordResetForm(request.POST, prefix='password_reset_')
+            if password_reset_form.is_valid():
+                context['new_password'] = password_reset_form.save()
     context['reputation_offset_form'] = reputation_offset_form
+    context['assign_student_form'] = assign_student_form
+    context['assign_teacher_form'] = assign_teacher_form
+    context['password_reset_form'] = password_reset_form
     context['sub_nav'] = [{'name': 'Utilizadores', 'url': reverse('management:users')}]
     return render(request, 'management/users.html', context)
 
