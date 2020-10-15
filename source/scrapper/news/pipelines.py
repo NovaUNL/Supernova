b09a2e13d44afa96ab3e844878085316e7850b15
Cláudio Pereira <git@claudiop.com>
2020-10-15 16:04:15 +0100

    Scrappers: News item pipeline adjustments.

--- source/scrapper/news/pipelines.py
+++ source/scrapper/news/pipelines.py
@@ -1,6 +1,8 @@
 import io
+import logging
 
 from django.core.files import File
+from django.db.models import Q
 
 from news.models import NewsItem
 
@@ -9,10 +11,12 @@ class NewsItemPipeline(object):
     def process_item(self, item, spider):
         image = item.get('image_data', None)
         image_filename = item.get('image_filename', None)
+
         current = NewsItem.objects.filter(
-            title=item['title'],
-            datetime=item['datetime'],
-            generated=True).first()
+            Q(title=item['title'], datetime=item['datetime']) | Q(source=item['source']), generated=True).first()
+        if current is not None:
+            print(f"{item['title']} - {item['datetime']}\n"
+                  f"\t {item['source']} {item.get('image_filename', None)}\n\tCurrent: {current} {current.cover_img}")
         if current is None:
             current = NewsItem(
                 title=item['title'],
@@ -24,8 +28,12 @@ class NewsItemPipeline(object):
         else:
             current.content = item['content']
 
+        if image_filename is not None:
+            if image is None:
+                logging.error("Inconsistency. Image name but no image.")
+                return
+            current.cover_img.save(image_filename, File(io.BytesIO(image)))
+
         current.gen_summary()
         current.save()
-        # if image is not None:
-        #     current.cover_img.save(image_filename, File(io.BytesIO(image)))
         return item
