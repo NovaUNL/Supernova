73d5353202ad358b60ab8b87534018cdb57c4441
Cláudio Pereira <git@claudiop.com>
2020-10-26 22:21:46 +0000

    Remove unused parameters in the college api's.

--- source/api/views/college.py
+++ source/api/views/college.py
@@ -14,32 +14,32 @@ from users.utils import get_students
 
 
 class BuildingList(APIView):
-    def get(self, request, format=None):
+    def get(self, request):
         serializer = serializers.BuildingSerializer(college.Building.objects.all(), many=True)
         return Response(serializer.data)
 
 
 class BuildingDetailed(APIView):
-    def get(self, request, pk, format=None):
+    def get(self, request, pk):
         serializer = serializers.BuildingSerializer(college.Building.objects.get(pk=pk))
         return Response(serializer.data)
 
 
 class DepartmentList(APIView):
-    def get(self, request, format=None):
+    def get(self, request):
         serializer = serializers.DepartmentMinimalSerializer(
             college.Department.objects.all(), many=True)
         return Response(serializer.data)
 
 
 class DepartmentDetailed(APIView):
-    def get(self, request, pk, format=None):
+    def get(self, request, pk):
         serializer = serializers.DepartmentSerializer(college.Department.objects.get(id=pk))
         return Response(serializer.data)
 
 
 class CourseDetailed(APIView):
-    def get(self, request, pk, format=None):
+    def get(self, request, pk):
         course = college.Course.objects.get(id=pk)
         data = serializers.CourseSerializer(course).data
         data['degree'] = course.degree.name
@@ -50,7 +50,7 @@ class ClassDetailed(APIView):
     authentication_classes = (SessionAuthentication, BasicAuthentication)
     permission_classes = (IsAuthenticated,)
 
-    def get(self, request, pk, format=None):
+    def get(self, request, pk):
         serializer = serializers.CourseSerializer(college.Class.objects.get(id=pk))
         return Response(serializer.data)
 
@@ -59,7 +59,7 @@ class UserShiftInstances(APIView):
     authentication_classes = (SessionAuthentication, BasicAuthentication)
     permission_classes = (IsAuthenticated,)
 
-    def get(self, request, nickname, format=None):
+    def get(self, request, nickname):
         user = get_object_or_404(users.User.objects.prefetch_related('students'), nickname=nickname)
         primary_students, _ = get_students(user)
 
