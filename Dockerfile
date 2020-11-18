f0c271806506c7fdd4c48c4a87c51f1b3a5fb708
Cláudio Pereira <git@claudiop.com>
2020-11-18 22:52:59 +0000

    ASGI adjustments.

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,16 @@ FROM python:3.8-buster
 
 LABEL maintainer="Cláudio Pereira <supernova@claudiop.com>"
 
-COPY . /
+# Could copy everything here instead of just pip-packages,
+# but this way is better for development as source changes do not affect cached image layers
+COPY pip-packages /
 RUN apt update \
-&& apt install  -y --no-install-recommends git sqlite libgdal20 \
+&& apt install  -y --no-install-recommends sqlite libgdal20 \
 && pip install --no-cache-dir --trusted-host pypi.python.org -r /pip-packages \
-&& rm -rf /var/lib/apt/lists/* /pip-packages \
-&& apt remove -y gcc
+&& rm -rf /var/lib/apt/lists/* /pip-packages
 
 VOLUME  ["/conf", "/http"]
 WORKDIR /source
 EXPOSE 1893
-ENTRYPOINT ["uwsgi", "/conf/uwsgi.ini"]
+ENTRYPOINT ["gunicorn", "-c", "/conf/gunicorn.conf.py", "asgi:application", "-k", "uvicorn.workers.UvicornWorker"]
+COPY . /
--- source/asgi.py
+++ source/asgi.py
@@ -1,21 +1,24 @@
 import os
 
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+django_asgi_app = get_asgi_application()
+
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "..settings")
-
-application = get_asgi_application()
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     "websocket": AllowedHostsOriginValidator(
-#         AuthMiddlewareStack(
-#             URLRouter(
-#                 chat.routing.websocket_urlpatterns
-#             )
-#         )
-#     ),
-# })
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
