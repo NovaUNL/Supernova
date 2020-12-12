301ae271a9099619cdc6b5c29d561fd29571e30f
Cláudio Pereira <git@claudiop.com>
2020-12-12 14:34:58 +0000

    Do not attempt to load notifications in no user is logged in.

--- source/static/js/init.js
+++ source/static/js/init.js
@@ -1,15 +1,19 @@
+const UID = JSON.parse($('#uid').text());
+
 (function () {
     loadTheme(true);
     let notifications = $('#notification-count');
-    fetch("/api/notification/count", {credentials: 'include'})
-        .then((response) => response.json())
-        .then((count) => {
-            if (count > 0) {
-                notifications.text(count).css('visibility', 'visible');
-                loadNotifications();
-                setupPopover();
-            }
-        });
+    if (UID > 0) {
+        fetch("/api/notification/count", {credentials: 'include'})
+            .then((response) => response.json())
+            .then((count) => {
+                if (count > 0) {
+                    notifications.text(count).css('visibility', 'visible');
+                    loadNotifications();
+                    setupPopover();
+                }
+            });
+    }
     fetch("/api/stars/gitlab/")
         .then((response) => response.json())
         .then((stars) => $('#gitlab-count').text(stars));
