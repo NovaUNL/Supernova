cde1f716eaca412314a4a0432bc3d748af5890f0
Cláudio Pereira <git@claudiop.com>
2020-12-14 12:25:10 +0000

    Include silk in the URL patterns.

--- source/supernova/urls.py
+++ source/supernova/urls.py
@@ -92,3 +92,4 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns = [path('__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
+    urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
