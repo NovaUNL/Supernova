e1711e7cfe31afbcc0d78bea28d5e0da0d20436c
Cláudio Pereira <git@claudiop.com>
2021-01-24 01:09:32 +0000

    Learning: Improve section API code.

--- source/api/views/learning.py
+++ source/api/views/learning.py
@@ -56,38 +56,42 @@ class SectionChildren(APIView):
 
     def post(self, request, pk, format=None):
         section = get_object_or_404(learning.Section, id=pk)
-        if isinstance(request.data, dict) \
-                and 'child' in request.data \
-                and isinstance((child_id := request.data['child']), int):
+        if not isinstance(request.data, dict):
+            raise ValidationError("Bad request")
 
-            with transaction.atomic():
-                child = get_object_or_404(learning.Section, id=child_id)
-                if child in section.children.all():
-                    return Response("Ok")
-                biggest_index = learning.SectionSubsection.objects \
-                    .filter(parent=section) \
-                    .annotate(num_books=Max('index')) \
-                    .aggregate(Max('index'))['index__max']
-                if biggest_index is None:
-                    biggest_index = -1
-                new = learning.SectionSubsection.objects.create(parent=section, section=child, index=biggest_index + 1)
-                return Response(SectionRelationSerializer(new).data)
-        raise ValidationError("Bad request")
+        child_id = request.data.get('child')
+        if not isinstance(child_id, int):
+            raise ValidationError("Bad request")
+
+        with transaction.atomic():
+            child = get_object_or_404(learning.Section, id=child_id)
+            if child in section.children.all():
+                return Response("Ok")
+            biggest_index = learning.SectionSubsection.objects \
+                .filter(parent=section) \
+                .annotate(num_books=Max('index')) \
+                .aggregate(Max('index'))['index__max']
+            if biggest_index is None:
+                biggest_index = -1
+            new = learning.SectionSubsection.objects.create(parent=section, section=child, index=biggest_index + 1)
+            return Response(SectionRelationSerializer(new).data)
 
     def delete(self, request, pk, format=None):
         section = get_object_or_404(learning.Section, id=pk)
-        if isinstance(request.data, dict) \
-                and 'child' in request.data \
-                and isinstance((child_id := request.data['child']), int):
-            with transaction.atomic():
-                rel = learning.SectionSubsection.objects.filter(parent=section, section_id=child_id).first()
-                old_index = rel.index
-                learning.SectionSubsection.objects.filter(parent=section, section_id=child_id).delete()
-                learning.SectionSubsection.objects \
-                    .filter(parent=section, index__gt=old_index) \
-                    .update(index=F('index') - 1)
-            return Response("Ok")
-        raise ValidationError("Bad request")
+        if not isinstance(request.data, dict):
+            raise ValidationError("Bad request")
+        child_id = request.data.get('child')
+        if not isinstance(child_id, int):
+            raise ValidationError("Bad request")
+
+        with transaction.atomic():
+            rel = learning.SectionSubsection.objects.filter(parent=section, section_id=child_id).first()
+            old_index = rel.index
+            learning.SectionSubsection.objects.filter(parent=section, section_id=child_id).delete()
+            learning.SectionSubsection.objects \
+                .filter(parent=section, index__gt=old_index) \
+                .update(index=F('index') - 1)
+        return Response("Ok")
 
     def put(self, request, pk, format=None):
         section = get_object_or_404(learning.Section, id=pk)
@@ -138,41 +142,44 @@ class ClassSections(APIView):
 
     def post(self, request, pk, format=None):
         class_ = get_object_or_404(college.Class, id=pk)
-        if isinstance(request.data, dict) \
-                and 'child' in request.data \
-                and isinstance((child_id := request.data['child']), int):
+        if not isinstance(request.data, dict):
+            raise ValidationError("Bad request")
+        child_id = request.data.get('child')
+        if not isinstance(child_id, int):
+            raise ValidationError("Bad request")
 
-            with transaction.atomic():
-                child = get_object_or_404(learning.Section, id=child_id)
-                if child in class_.synopsis_sections.all():
-                    return Response("Ok")
-                biggest_index = learning.ClassSection.objects \
-                    .filter(corresponding_class=class_) \
-                    .annotate(num_books=Max('index')) \
-                    .aggregate(Max('index'))['index__max']
-                if biggest_index is None:
-                    biggest_index = -1
-                new = learning.ClassSection.objects.create(
-                    corresponding_class=class_,
-                    section=child,
-                    index=biggest_index + 1)
-                return Response(SectionRelationSerializer(new).data)
-        raise ValidationError("Bad request")
+        with transaction.atomic():
+            child = get_object_or_404(learning.Section, id=child_id)
+            if child in class_.synopsis_sections.all():
+                return Response("Ok")
+            biggest_index = learning.ClassSection.objects \
+                .filter(corresponding_class=class_) \
+                .annotate(num_books=Max('index')) \
+                .aggregate(Max('index'))['index__max']
+            if biggest_index is None:
+                biggest_index = -1
+            new = learning.ClassSection.objects.create(
+                corresponding_class=class_,
+                section=child,
+                index=biggest_index + 1)
+            return Response(SectionRelationSerializer(new).data)
 
     def delete(self, request, pk, format=None):
         class_ = get_object_or_404(college.Class, id=pk)
-        if isinstance(request.data, dict) \
-                and 'child' in request.data \
-                and isinstance((child_id := request.data['child']), int):
-            with transaction.atomic():
-                rel = learning.ClassSection.objects.filter(corresponding_class=class_, section_id=child_id).first()
-                old_index = rel.index
-                learning.ClassSection.objects.filter(corresponding_class=class_, section_id=child_id).delete()
-                learning.ClassSection.objects \
-                    .filter(corresponding_class=class_, index__gt=old_index) \
-                    .update(index=F('index') - 1)
-            return Response("Ok")
-        raise ValidationError("Bad request")
+        if not isinstance(request.data, dict):
+            raise ValidationError("Bad request")
+        child_id = request.data.get('child')
+        if not isinstance(child_id, int):
+            raise ValidationError("Bad request")
+
+        with transaction.atomic():
+            rel = learning.ClassSection.objects.filter(corresponding_class=class_, section_id=child_id).first()
+            old_index = rel.index
+            learning.ClassSection.objects.filter(corresponding_class=class_, section_id=child_id).delete()
+            learning.ClassSection.objects \
+                .filter(corresponding_class=class_, index__gt=old_index) \
+                .update(index=F('index') - 1)
+        return Response("Ok")
 
     def put(self, request, pk, format=None):
         class_ = get_object_or_404(college.Class, id=pk)
@@ -221,7 +228,7 @@ class QuestionUserVotes(APIView):
             .filter(question__activity_id=question.activity_id, user=request.user) \
             .values_list('type', flat=True)
         answer_votes = feedback.Vote.objects \
-            .filter(answer__to__activity_id=question.activity_id, user=request.user)\
+            .filter(answer__to__activity_id=question.activity_id, user=request.user) \
             .order_by('object_id') \
             .values_list('object_id', 'type')
         last_postable = None
